ef190460068dda96377d19905a39660580476f39
Kambing-tersesat <163005623+Kambing-tersesat@users.noreply.github.com>
2024-08-10 20:07:42 +0700

    Add files via upload
    Convert month to string

--- asset/date.js
+++ asset/date.js
@@ -1,51 +1,36 @@
 function updateTime() {
+  const now = new Date();
 
-const now = new Date();
+  const year = now.getFullYear();
+  const month = now.toLocaleString('default', { month: 'long' });
+  const day = now.getDate();
+  const hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2, "0");
 
-
-const year = now.getFullYear();
-
-const month = now.getMonth() + 1;
-
-const day = now.getDate();
-
-const hours = now.getHours();
-
-const minutes = String(now.getMinutes()).padStart(2, "0");
-
-const seconds = String(now.getSeconds()).padStart(2, "0");
-
-
-let greeting;
-  if (hours === 5 && minutes < 60) {
+  let greeting;
+  if (hours < 5) {
     greeting = "Wake up Sleepyhead";
-  } else if (hours >= 6 && hours < 12) {
+  }
+  else if (hours < 6) {
     greeting = "Good morning";
-  } else if (hours >= 12 && hours < 18) {
+  }
+   else if (hours < 12) {
     greeting = "Good afternoon";
-  } else if (hours >= 18 && hours < 22) {
+  } else if (hours <= 18){
     greeting = "Good evening";
-  } else if (hours === 22) {
-    greeting = "Still awake ?, Go to sleep dumbass";
-  } else if (hours >= 23 || hours < 4) {
-    greeting = "Good Night, Rawwwrr";
-  } else if (hours === 4) {
-    greeting = "The Dawn Will Rise";
+  }
+  else if(hours >= 22){
+    greeting = "Still awake ?, go to bed baby"
+  }
+  else{
+    greeting = "Good Night, Rawwwrr"
   }
 
-const greets = `${greeting}`;
+  const greets = `${greeting}`;
+  const formattedTime = `it is ${hours}:${minutes} on ${month}/${day}/${year}`;
 
-const formattedTime = `it is ${hours}:${minutes}:${seconds} on ${month}/${day}/${year}`;
-
-
-document.getElementById("time").innerHTML = formattedTime;
-
-document.getElementById("greet").innerHTML = greeting;
-
+  document.getElementById("time").innerHTML = formattedTime;
+  document.getElementById("greet").innerHTML = greeting;
 }
-
 updateTime();
-
 setInterval(updateTime, 1000);
-
-  
